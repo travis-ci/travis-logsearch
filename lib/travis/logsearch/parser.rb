78dceaf263f3165bb00aa363b02be83d3e766b2e
Igor Wiedler <igor@travis-ci.org>
2018-09-14 13:51:04 +0200

    add missing file

---
+++ lib/travis/logsearch/parser.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+module Travis
+  module LogSearch
+    class Parser
+      def folds(nodes)
+        nodes
+          .select { |n| n[:type] == :fold }
+          .map { |n| [n[:name], n[:body].strip] }
+          .to_h
+          .tap { |h|
+            h.delete('system_info');
+            h.delete('rvm');
+            h.delete('ruby.versions')
+          }
+      end
+
+      def text(nodes)
+        nodes
+          .select { |n| n[:type] == :text }
+          .map { |n| n[:body].strip }
+          .join("\n\n")
+      end
+
+      def parse(log)
+        log
+          .scan(/(?:travis_fold:start:(.+?)\r(.+?)travis_fold:end:(.+?)|(.+?)(?=travis_fold:start|\z))/m)
+          .map { |m|
+            if m[0]
+              { type: :fold, name: m[0], body: m[1] }
+            else
+              { type: :text, body: m[3] }
+            end
+          }
+          .map { |n|
+            n[:body] = n[:body]
+              .gsub(/\r\r/, "\r")
+              .gsub(/^.*\r(?!$)/, '')
+              .gsub(/\x1b(\[|\(|\))[;?0-9]*[0-9A-Za-z]/m, '')
+              .gsub(/\r\n/, "\n")
+              .gsub(/\r/, '')
+            n
+          }
+          .select { |n| n[:body] != '' }
+      end
+    end
+  end
+end
