21952bf4340b5df0263aa9699552cc5ffad88155
Igor Wiedler <igor@travis-ci.org>
2018-09-14 11:52:56 +0200

    reduce number of replicas to 0 so that we can save space (at the expense of reliability)

--- setup.rb
+++ setup.rb
@@ -15,7 +15,7 @@ es.indices.put_template(
     settings: {
       index: {
         number_of_shards: 5,
-        number_of_replicas: 1,
+        number_of_replicas: 0,
         highlight: {
           max_analyzed_offset: 5000000
         }
